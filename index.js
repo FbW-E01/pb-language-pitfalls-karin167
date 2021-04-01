bc167971415d427254f37d1af07e8b450fb49db8
karin yahud <karin167bz@gmail.com>
2021-04-01 13:36:35 +0200

    karin-task

---
+++ index.js
@@ -0,0 +1,29 @@
+//1
+const hello = "hello";
+console.log(isNaN(hello));
+
+const num = 3;
+console.log(isNaN(num));
+console.log(typeof NaN);
+
+//2
+
+const foo = 0.1;
+const goo = 0.2;
+const doo = foo * goo
+console.log(foo * goo);
+console.log(doo.toFixed(2));
+
+
+//3
+const zero = 0;
+console.log( Infinity / zero);
+console.log( Infinity / Infinity);
+console.log( 1 / zero);
+
+
+
+
+
+
+
